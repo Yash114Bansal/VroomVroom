145a86f3001df628588c7c4e620a482e3a468ab0
yash114bansal <yash114bansal@gmail.com>
2024-04-07 01:06:12 +0530

    add push notifications

---
+++ accounts/utils.py
@@ -0,0 +1,28 @@
+import requests
+from django.conf import settings
+
+def send_push_notification(title, body, users):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"key={settings.FCM_API_KEY}"
+    }
+    
+    for user in users:
+        title_formatted = title.replace("{name}", user.name)
+        body_formatted = body.replace("{name}", user.name)
+        
+        if user.fcm_token:
+            data = {
+                "to": user.fcm_token,
+                "notification": {
+                    "title": title_formatted,
+                    "body": body_formatted,
+                }
+            }
+        
+            try:
+                requests.post("https://fcm.googleapis.com/fcm/send", json=data, headers=headers)
+                
+            except requests.RequestException as e:
+                
+                print(f"Error sending notification to {user.name}: {e}")
